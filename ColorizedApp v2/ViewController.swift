13d07bccb2a3abb27088bf3158c76f11ab12b4bd
lsvard <l9244492@bk.ru>
2023-08-12 14:34:10 +0300

    Work with logic

--- ColorizedApp v2/ViewController.swift
+++ ColorizedApp v2/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet var mainView: UIView!
+final class ViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var colorView: UIView!
     
     @IBOutlet var redLabel: UILabel!
     @IBOutlet var greenLabel: UILabel!
@@ -19,14 +20,34 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        colorView.layer.cornerRadius = 15
     }
-
-
-    @IBAction func skidersAction() {
+    
+    // MARK: - IBActions
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: sender)
+        case greenSlider:
+            greenLabel.text = string(from: sender)
+        default:
+            blueLabel.text = string(from: sender)
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2F", slider.value)
     }
 }
-
