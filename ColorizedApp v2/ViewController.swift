687f3a74eef93268595a501ee0658d2d6cc2f588
lsvard <l9244492@bk.ru>
2023-08-12 09:32:56 +0300

    Work with storyboard

--- ColorizedApp v2/ViewController.swift
+++ ColorizedApp v2/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
 
+    @IBAction func skidersAction() {
+    }
 }
 
